7788fcc2a8db38e13e09a28c2333229eb926ca82
PaoloRollo <paolo.rollo1997@gmail.com>
2024-09-07 17:02:17 +0200

    feat: update.

---
+++ lib/db/records.ts
@@ -0,0 +1,41 @@
+import { prisma } from "./index";
+import { createThirdwebClient } from "thirdweb";
+import { upload } from "thirdweb/storage";
+
+const generateHTML = (slug: string) =>
+  `<html>
+    <head>
+     <meta http-equiv="refresh" content="0; URL=https://flux.builders.garden/pay/${slug}" />
+   </head>
+  </html>
+`;
+
+export const createRecord = async (slug: string, address: string) => {
+  try {
+    const client = createThirdwebClient({
+      secretKey: process.env.THIRDWEB_SECRET_KEY!,
+    });
+
+    const campaignURI = await upload({
+      client,
+      files: [new File([generateHTML(slug)], `index.html`)],
+    });
+
+    const newRecord = await prisma.record.create({
+      data: {
+        owner: address,
+        name: `${slug}.fluxlink.eth`,
+        addresses: {},
+        texts: {},
+        contenthash: campaignURI,
+      },
+    });
+
+    return newRecord;
+  } catch (error) {
+    console.error("Error creating ENS record:", error);
+    throw error;
+  } finally {
+    await prisma.$disconnect();
+  }
+};
