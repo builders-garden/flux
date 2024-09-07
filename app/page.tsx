89207b34520695fb80b06b7071fd43cab0cfec0f
lim0n3 <simonestaffa96@gmail.com>
2024-09-07 09:48:25 +0200

    Landing page

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,22 @@
+import { Button } from "@nextui-org/react";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <div className="text-3xl">CIAO</div>
+    <div className="flex flex-row justify-center items-center justify-items-center min-h-screen p-24 pb-20 gap-16 sm:p-20]">
+      <div className="flex flex-row justify-center">
+        <div className="w-1/2 flex flex-col gap-4">
+          <div className="text-6xl font-black">
+            Financial infrastructure to grow your revenue
+          </div>
+          <div className="text-lg">
+            Join the millions of companies of all sizes that use Stripe to
+            accept payments online and in person, embed financial services,
+            power custom revenue models, and build a more profitable business.
+          </div>
+          <Button radius="full">Start now</Button>
+        </div>
+        <div className="w-1/2"></div>
+      </div>
     </div>
   );
 }
