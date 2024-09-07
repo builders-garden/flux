cd9cc04355dd0f6e644755e5f6633b9677ade38d
lim0n3 <simonestaffa96@gmail.com>
2024-09-07 10:06:38 +0200

    change colors

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
     <div className="flex flex-row justify-center items-center justify-items-center min-h-screen p-24 pb-20 gap-16 sm:p-20]">
       <div className="flex flex-row justify-center">
         <div className="w-1/2 flex flex-col gap-4">
-          <div className="text-6xl font-black">
+          <div className="text-6xl font-black text-primary">
             Financial infrastructure to grow your revenue
           </div>
           <div className="text-lg">
@@ -13,7 +13,9 @@ export default function Home() {
             accept payments online and in person, embed financial services,
             power custom revenue models, and build a more profitable business.
           </div>
-          <Button radius="full">Start now</Button>
+          <Button radius="full" color="primary">
+            Start now
+          </Button>
         </div>
         <div className="w-1/2"></div>
       </div>
