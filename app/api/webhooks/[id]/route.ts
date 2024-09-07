2c4ec952f92888901851d5592fc48709551e5fe0
lim0n3 <simonestaffa96@gmail.com>
2024-09-07 13:27:15 +0200

    Webhooks endpoints

---
+++ app/api/webhooks/[id]/route.ts
@@ -0,0 +1,44 @@
+import { getUserByAddress } from "@/lib/db/users";
+import { getWebhookById, deleteWebhook, updateWebhook } from "@/lib/db/webhook";
+import { NextResponse } from "next/server";
+
+export const DELETE = async (
+  req: Request,
+  { params }: { params: { id: string } }
+) => {
+  const { id } = params;
+  const address = req.headers.get("x-address")!;
+  const user = await getUserByAddress(address)!;
+  if (!user) {
+    return NextResponse.json({ error: "User not found" }, { status: 404 });
+  }
+  const webhook = await getWebhookById(id);
+  if (!webhook || webhook.userId !== user.id) {
+    return NextResponse.json({ error: "Webhook not found" }, { status: 404 });
+  }
+  await deleteWebhook(id);
+  return NextResponse.json({ message: "Webhook deleted" });
+};
+
+export const PUT = async (
+  req: Request,
+  { params }: { params: { id: string } }
+) => {
+  const { id } = params;
+  const address = req.headers.get("x-address")!;
+  const user = await getUserByAddress(address)!;
+  if (!user) {
+    return NextResponse.json({ error: "User not found" }, { status: 404 });
+  }
+  const webhook = await getWebhookById(id);
+  if (!webhook || webhook.userId !== user.id) {
+    return NextResponse.json({ error: "Webhook not found" }, { status: 404 });
+  }
+  const { name, url, event } = await req.json();
+  await updateWebhook(id, { 
+    ...(name && { name }),  
+    ...(url && { url }),
+    ...(event && { event }),
+  });
+  return NextResponse.json({ message: "Webhook updated" });
+};
---
+++ app/api/webhooks/route.ts
@@ -0,0 +1,44 @@
+import { getUserByAddress } from "@/lib/db/users";
+import { createWebhook, getWebhooksByUserId } from "@/lib/db/webhook";
+import { NextResponse } from "next/server";
+
+export const POST = async (req: Request) => {
+  const address = req.headers.get("x-address")!;
+
+  const user = await getUserByAddress(address)!;
+
+  if (!user) {
+    return NextResponse.json({ error: "User not found" }, { status: 404 });
+  }
+
+  const body = await req.json();
+
+  const { name, url, event } = body;
+
+  const webhook = await createWebhook(user.id, { name, url, event });
+
+  return NextResponse.json(webhook);
+};
+
+export const GET = async (req: Request) => {
+  const address = req.headers.get("x-address")!;
+  const { searchParams } = new URL(req.url);
+  const limit = searchParams.get("limit")
+    ? parseInt(searchParams.get("limit")!)
+    : 10;
+  const page = searchParams.get("page")
+    ? parseInt(searchParams.get("page")!)
+    : 0;
+  const user = await getUserByAddress(address)!;
+
+  if (!user) {
+    return NextResponse.json({ error: "User not found" }, { status: 404 });
+  }
+
+  const webhooks = await getWebhooksByUserId(user.id, {
+    limit,
+    offset: page * limit,
+  });
+
+  return NextResponse.json(webhooks);
+};
